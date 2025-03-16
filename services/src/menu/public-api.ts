ea5352901756c39ecc44c48e124ea917af612242
thanhtung1010 <trinhthanhtung1010@gmail.com>
2025-03-16 22:06:17 +0700

    missing import

--- services/src/menu/public-api.ts
+++ services/src/menu/public-api.ts
@@ -2,3 +2,5 @@ export * from './menu.dto';
 export * from './menu.enum';
 export * from './menu.interface';
 export * from './menu.service';
+export * from './menu.provider';
+export * from './menu.module';
